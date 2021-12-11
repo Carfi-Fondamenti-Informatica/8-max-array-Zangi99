4575680bf8ef767b27467d75f63077c98369215e
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-12-11 11:12:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+void MaxV(int d, float V[d]){
+    float max;
+    max=V[0];
+    for (int i=0; i<d; i++){
+        if(max<V[(i+1)]){
+            max=V[(i+1)];
+        }
+    }cout<<max;
+}
+
+int main() {
+
+    int d=0;
+    cin>>d;
+
+    float V[d];
+    for(int i=0; i<d; i++){
+        cin>>V[i];
+    }
+    MaxV(d,V);
+
+    return 0;
 }
